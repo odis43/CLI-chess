c4fbb2eef4c355e324be97094db866722bf104e3
gpineda2003 <117306085+gpineda2003@users.noreply.github.com>
2022-11-27 18:14:11 -0500

    Add files via upload

---
+++ Player.cc
@@ -0,0 +1,31 @@
+#include "Player.h"
+
+#include <vector>
+#include "Piece.h"
+
+Player::Player(int side) : board{nullptr}, pieces{vector<Piece*>()}, side{side}, resign{false} {}
+
+Player::~Player() {}
+
+void Player::addPiece(Piece *piece) {
+    pieces.emplace_back(piece);
+}
+
+/*
+void Player::removePiece(Piece *piece) {
+    pieces.emplace_back(piece);
+}
+*/
+
+bool Player::own(Piece *piece) {
+    for (int i = 0; i < pieces.size(); i++) {
+        if (piece == pieces[i]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+int Player::getSide() { return side; }
+
+bool Player::getResign() { return resign; }
---
+++ Player.h
@@ -0,0 +1,31 @@
+#ifndef _PLAYER_H_
+#define _PLAYER_H_
+
+#include <vector>
+#include <memory>
+
+class Piece;
+class Board;
+
+class Player {
+private:
+    Board *board; // holds game board
+    std::vector<Piece *> pieces; // holds pieces owned by player
+    int side; // (used to be called colour), side makes more sense for 4 player chess, also side is either 1, 2, 3, or 4.
+    bool resign; // true if player has resigned
+
+    virtual std::vector<int> moveCreate() = 0; // function to move player piece
+
+public:
+    Player(int side);
+    
+    void addPiece(Piece *); // adds pieces
+    //void removePiece(Piece *); // removes pieces
+    std::vector<Piece *> getPieces(); // returns pieces
+    bool own(Piece *); // returns true if piece is owned by Player object
+
+    int getSide(); // returns side
+    bool getResign(); // returns resign
+
+};
+#endif
