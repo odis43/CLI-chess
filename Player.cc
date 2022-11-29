fc675a223c051602b1c3e510eeef480a7232d099
gpineda <gpineda@uwaterloo.ca>
2022-11-29 17:13:24 -0500

    changes to Player

--- Player.cc
+++ Player.cc
@@ -2,19 +2,22 @@
 #include "Player.h"
 #include "Piece.h"
 
+// Constructor
 Player::Player(std::string colour) : board{nullptr}, pieces{vector<Piece*>()}, colour{colour}, resign{false} {}
 
+// Destructor
 Player::~Player() {}
 
+// Adds a piece to back of vector which holds Player's owned pieces
 void Player::addPiece(Piece *piece) {
     pieces.emplace_back(piece);
 }
 
-/*
+// Removes a piece from player's owned pieces
 void Player::removePiece(Piece *piece) {
     pieces.emplace_back(piece);
 }
-*/
+
 
 bool Player::own(Piece *piece) {
     for (int i = 0; i < pieces.size(); i++) {
