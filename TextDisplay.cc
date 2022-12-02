f5302f4ffaaa9a47c4a8042a7f62200871c659a5
gpineda <gpineda@uwaterloo.ca>
2022-12-01 19:40:57 -0500

    TextDisplay rough

--- TextDisplay.cc
+++ TextDisplay.cc
@@ -11,5 +11,35 @@ TextDisplay::TextDisplay(Board *board) : board{board} {}
 // Print board
 void TextDisplay::printBoard() {
     vector<vector<Tile *>> chessBoard = board->getBoardRef();
+    cout << " ";
+    for (int i = 0; i < 8; i++) { // print out column headers
+        char character = i + 97;
+        cout << " " << character << " ";
+    }
+    cout << endl;
+    for (int i = 0; i < 8; i++) {
+        cout << "  "; // place some padding
+        for (int j = 0; j < 8; j++) {
+            cout << "-----";
+        }
+        cout << endl;
+        cout << 8 - i << " "; // print row number
+        for (int j = 0; j < 8; j++) {
+            auto boardTile = chessBoard[i][j];
+            cout << "| ";
+            if (boardTile->getPiece()) {
+                boardTile->getPiece()->printPiece();
+            } else {
+                cout << " ";
+            }
+            cout << " |";
+        }
+        cout << endl;
+    }
+    cout << "   "; // add some more padding
+    for (int i = 0; i < 8; i++) {
+        cout << "-----";
+    }
+    cout << endl;
 }
 
