aba80b28f232ff7656ee76b24c65f0e14b0ad16e
gpineda <gpineda@uwaterloo.ca>
2022-12-02 18:12:58 -0500

    small additions

--- Rook.cc
+++ Rook.cc
@@ -1,24 +1,24 @@
 #include "Rook.h"
 #include "Tile.h"
-#include "Rook.h"
+#include "Piece.h"
 
 using namespace std;
 
-Rook::Rook(string colour):Piece{colour, 5}, {}
+Rook::Rook(string colour):Piece{colour, 5} {}
 
-Rook::~Rook(){}
+Rook::~Rook() {}
 
 void Rook::print() {
     if(getColour() == "white") cout << "R";
     if(getColour() == "black") cout << "r";
 }
 
- void moveUp(int row, int col, std::vector<std::vector<Tile*>> board, Tile *tile) {
+void moveUp(int row, int col, std::vector<std::vector<Tile*>> board, Tile *tile) {
     int newRow = row + 1;
     while(newRow < 8) {
         Piece *tilePiece = board[newRow][col]->getPiece();
         if(tilePiece) {
-            if(tilePiece ->getColour() == getColour()) {
+            if(tilePiece->getColour() == getColour()) {
                 updateValidMoves(board[newRow][col],3);
                 break;
             } else {
@@ -30,9 +30,9 @@ void Rook::print() {
             newRow++;
         }
     }
- }
+}
 
- void moveDown(int row, int col, std::vector<std::vector<Tile*>> board, Tile *tile) {
+void moveDown(int row, int col, std::vector<std::vector<Tile*>> board, Tile *tile) {
     int newRow = row - 1;
     while(newRow >= 0) {
         Piece *tilePiece = board[newRow][col]->getPiece();
@@ -49,9 +49,9 @@ void Rook::print() {
             newRow--;
         }
     }
- }
+}
 
- void moveRight(int row, int col, std::vector<std::vector<Tile*>> board, Tile *tile) {
+void moveRight(int row, int col, std::vector<std::vector<Tile*>> board, Tile *tile) {
     int newCol = col + 1;
     while(newCol < 8) {
         Piece *tilePiece = board[row][newCol]->getPiece();
@@ -70,7 +70,7 @@ void Rook::print() {
     }
  }
 
- void moveRight(int row, int col, std::vector<std::vector<Tile*>> board, Tile *tile) {
+void moveRight(int row, int col, std::vector<std::vector<Tile*>> board, Tile *tile) {
     int newCol = col - 1;
     while(newCol >= 0) {
         Piece *tilePiece = board[row][newCol]->getPiece();
@@ -87,7 +87,7 @@ void Rook::print() {
             newCol--;
         }
     }
- }
+}
 
 void genMove(int row, int col, std::vector<std::vector<Tile*>> board, Tile *tile) {
     moveUp(row, col, board, tile);
