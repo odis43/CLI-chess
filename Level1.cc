0d958f13a9f1d45a26431355436da5d416cf05cd
gpineda <gpineda@uwaterloo.ca>
2022-12-05 18:38:10 -0500

    sc

--- Level1.cc
+++ Level1.cc
@@ -17,6 +17,7 @@ Level1::~Level1() {}
 
 // Creates move info to place into a Move object
 std::vector<int> Level1::moveCreate() {
+    srand(time(0)); // generate seed for rand()
     vector<vector<Tile *>> curBoard = getBoard()->getBoardRef(); // holds a reference to the board
     string input; // will store input provided
     while (getline(cin, input)) {
@@ -36,38 +37,37 @@ std::vector<int> Level1::moveCreate() {
                     }
                 }
             }
+            
+            while (1) {
+                int randomVal = rand() % myPieces.size(); // generate a random value within the number of pieces in myPieces
+                Piece *chosenPiece = myPieces.at(randomVal); // select the random piece with the given index from randomVal
 
-            srand(time(0));
-            int randomVal = rand() % myPieces.size(); // generate a random value within the number of pieces in myPieces
-            cout << randomVal << endl;
-            Piece *chosenPiece = myPieces.at(randomVal); // select the random piece with the given index from randomVal
+                Tile *initialTile = chosenPiece->getTile(); // this is the tile where the chosen piece is on
+                int rowInitialTile = initialTile->getRow(); // get row of this chosen piece's tile
+                int colInitialTile = initialTile->getCol(); // get col of this chosen piece's tile
 
-            Tile *initialTile = chosenPiece->getTile(); // this is the tile where the chosen piece is on
-            int rowInitialTile = initialTile->getRow(); // get row of this chosen piece's tile
-            int colInitialTile = initialTile->getCol(); // get col of this chosen piece's tile
+                Tile *destinationTile = chosenPiece->getRandomMove(); // this is the tile where our move will land on
+                int rowDestinationTile = destinationTile->getRow(); // get row of the destination tile
+                int colDestinationTile = destinationTile->getCol(); // get col of the destination tile
 
-            Tile *destinationTile = chosenPiece->getRandomMove(); // this is the tile where our move will land on
-            int rowDestinationTile = destinationTile->getRow(); // get row of the destination tile
-            int colDestinationTile = destinationTile->getCol(); // get col of the destination tile
+                try { // now let's actually commit to the move
+                    initialTile->remove(); // first remove the piece at our initial tile because it is moving
+                    Piece *previousPiece = destinationTile->getPiece(); // save the piece of the piece that was previously on the destination tile
+                    destinationTile->set(chosenPiece); // because our chosen piece will now live on the destination tile
+                    chosenPiece->setTile(destinationTile); // because our chosen piece will belong to destination tile
+                    chosenPiece->setNotMoved(false); // because piece has moved
 
-            try { // now let's actually commit to the move
-                initialTile->remove(); // first remove the piece at our initial tile because it is moving
-                Piece *previousPiece = destinationTile->getPiece(); // save the piece of the piece that was previously on the destination tile
-                destinationTile->set(chosenPiece); // because our chosen piece will now live on the destination tile
-                chosenPiece->setTile(destinationTile); // because our chosen piece will belong to destination tile
-                chosenPiece->setNotMoved(false); // because piece has moved
+                    // check for an existing captured piece and respond accordingly
+                    if (previousPiece) {
+                        return vector<int>{previousPiece->getTracker(), chosenPiece->getTracker(), rowInitialTile, colInitialTile, rowDestinationTile, colDestinationTile};
+                    } else {
+                        return vector<int>{0, chosenPiece->getTracker(), rowInitialTile, colInitialTile, rowDestinationTile, colDestinationTile};
+                    }
 
-                // check for an existing captured piece and respond accordingly
-                if (previousPiece) {
-                    return vector<int>{previousPiece->getTracker(), chosenPiece->getTracker(), rowInitialTile, colInitialTile, rowDestinationTile, colDestinationTile};
-                } else {
-                    return vector<int>{0, chosenPiece->getTracker(), rowInitialTile, colInitialTile, rowDestinationTile, colDestinationTile};
+                } catch (const out_of_range &e) {
+                    // if no valid move is provided
+                    cerr << "Invalid Move. " << e.what() << endl;
                 }
-
-            } catch (const out_of_range &e) {
-                // if no valid move is provided
-                cerr << "Invalid Move. " << e.what() << endl;
-                moveCreate();
             }
         }
     }
